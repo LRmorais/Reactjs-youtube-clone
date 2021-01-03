fb2e2ff58983dbac3fa398bf9e1a762bca02774e
Lucas Morais <lucas.morais23ti@gmail.com>
2021-01-03 00:14:29 -0300

    add AppBar

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
 // makeStyles cria um objeto de estilos que pode ser usado em cada elemento
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core'
 
-const useStyles = makeStyles({
+// importando propriedades definidas em theme
+const useStyles = makeStyles((theme) => ({
     root: {
-      background: 'red',
       height: '100vh'
     }
-  })
+  }));
 
 function Home() {
     // instaciando o objeto makeStyles
   const classes = useStyles();
   return (
       <div className={classes.root}>
-          Oii
+          <AppBar>
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      {/* <MenuIcon /> */}
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
       </div>
   )
 }
